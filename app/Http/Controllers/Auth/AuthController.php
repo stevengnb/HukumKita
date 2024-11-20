ce8b69f6f9f20e5a7c81de7cde87b02027365a8b
blogstevens@gmail.com <blogstevens@gmail.com>
2024-11-20 23:43:09 +0700

    add auth controller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller
 {
     //
+    public function login() {
+        return view('login');
+    }
+
+    public function register() {
+        return view('register');
+    }
+
     public function create(array $data) {
         return User::create([
             'name' => $data['name'],
