5ba49cc4b91fbea2a64487536909254ad9a45fce
Yoseftian Nuhedward <yoseftiannuhedward@gmail.com>
2024-11-29 14:55:59 +0700

    add: article page

---
+++ resources/views/article.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('title', 'Law Articles')
+
+@section('content')
+    <div style="display: flex; flex-direction: row; margin-bottom: 2rem;">
+        <input class="form-control mr-sm-2" style="" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" style="" type="submit">Search</button>
+    </div>
+
+    {{-- Grid 4 --}}
+    <h1>Popular Article</h1>
+    <section>
+        
+    </section>
+@endsection
