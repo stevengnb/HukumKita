1c29f4ac8b11579d29c35ad2db39f5cb18c33dcf
blogstevens@gmail.com <blogstevens@gmail.com>
2024-12-12 17:05:56 +0700

    add middleware to route

--- routes/web.php
+++ routes/web.php
@@ -9,20 +9,33 @@ use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\LawyerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'home'])->middleware('auth:web,lawyer')->name('home');
-Route::get('/articles', [ArticleController::class, 'showArticles'])->name('showArticles');
-Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
-Route::get('/articles/{id}', [ArticleController::class, 'showDetail'])->name('showArticleDetail');
-Route::get('/lawyers/{id}', [LawyerController::class, 'getLawyer'])->name('getLawyer');
-Route::get('/lawyers/booking/{id}', [LawyerController::class, 'getLawyerBookingPage'])->name('getLawyerBooking');
-Route::post('/appointments', [AppointmentController::class, 'store'])->name('appointments.store');
-Route::post('/update-rating-review/{userId}/{lawyerId}', [AppointmentController::class, 'updateRatingReview'])->name('updateRatingReview');
-Route::post('/comment', [CommentController::class, 'store'])->name('comments.store');
-Route::delete('/comment/{id}', [CommentController::class, 'delete'])->name('comments.delete');
-Route::get('/profile', [AuthController::class, 'profile'])->name('profile');
-Route::get('lawyer/myappointments', [AppointmentController::class, 'getLawyerAppointments'])->name('lawyer.appointments');
-Route::put('lawyer/myappointments/{userId}/{lawyerId}/{newStatus}', [AppointmentController::class, 'updateAppointmentStatus'])->name('updateAppointmentStatus');
-Route::get('locale/{lang}', [LanguageController::class, 'change'])->name('changeLang');
+Route::middleware(['auth:web,lawyer'])->group(function () {
+    Route::get('/', [HomeController::class, 'home'])->name('home');
+    Route::get('/articles', [ArticleController::class, 'showArticles'])->name('showArticles');
+    Route::get('/articles/{id}', [ArticleController::class, 'showDetail'])->name('showArticleDetail');
+    Route::get('/lawyers/{id}', [LawyerController::class, 'getLawyer'])->name('getLawyer');
+    Route::get('/profile', [AuthController::class, 'profile'])->name('profile');
+
+});
+
+Route::middleware(['auth:lawyer'])->group(function () {
+    Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
+    Route::get('/lawyers/booking/{id}', [LawyerController::class, 'getLawyerBookingPage'])->name('getLawyerBooking');
+    Route::get('lawyer/myappointments', [AppointmentController::class, 'getLawyerAppointments'])->name('lawyer.appointments');
+    Route::put('lawyer/myappointments/{userId}/{lawyerId}/{newStatus}', [AppointmentController::class, 'updateAppointmentStatus'])->name('updateAppointmentStatus');
+    Route::delete('/lawyer/delete-lawyer', [AuthController::class, 'deleteLawyer'])->name('lawyer.deleteAccount');
+
+});
+
+Route::middleware(['auth:web'])->group(function () {
+    Route::post('/appointments', [AppointmentController::class, 'store'])->name('appointments.store');
+    Route::post('/update-rating-review/{userId}/{lawyerId}', [AppointmentController::class, 'updateRatingReview'])->name('updateRatingReview');
+    Route::post('/comment', [CommentController::class, 'store'])->name('comments.store');
+    Route::delete('/comment/{id}', [CommentController::class, 'delete'])->name('comments.delete');
+    Route::delete('/delete-user', [AuthController::class, 'deleteUser'])->name('user.deleteAccount');
+
+});
+
 
 // user auth
 Route::get('login', [AuthController::class, 'login'])->name('login');
@@ -32,6 +45,7 @@ Route::post('register-process', [AuthController::class, 'registerProcess'])->nam
 Route::get('lawyers', [LawyerController::class, 'getLawyers'])->name('getLawyers');
 Route::get('myappointments', [AppointmentController::class, 'getUserAppointments'])->name('user.appointments');
 Route::post('/changePassword', [AuthController::class, 'changePasswordUser'])->name('changePassword');
+
 // lawyer auth
 Route::prefix('lawyer')->name('lawyer.')->group(function () {
     Route::get('login', [AuthController::class, 'loginLawyer'])->name('login');
@@ -40,9 +54,6 @@ Route::prefix('lawyer')->name('lawyer.')->group(function () {
     Route::post('register-process', [AuthController::class, 'registerProcessLawyer'])->name('register.process');
     Route::post('changePassword', [AuthController::class, 'changePasswordLawyer'])->name('changePassword');
 });
-Route::delete('/lawyer/delete-lawyer', [AuthController::class, 'deleteLawyer'])->name('lawyer.deleteAccount');
-Route::delete('/lawyer/delete-user', [AuthController::class, 'deleteUser'])->name('user.deleteAccount');
+
+Route::get('locale/{lang}', [LanguageController::class, 'change'])->name('changeLang');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-
-// Route::get('lawyer/{id}', [LawyerController::class, 'getLawyer'])->name('getLawyer');
